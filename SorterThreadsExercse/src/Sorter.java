63c38a810f81ae50ba2028eff0b7af52b9502bdb
Kristen Foong <kristen.foong@gmail.com>
2019-03-27 12:48:43 -0700

    updates

---
+++ SorterThreadsExercse/src/Sorter.java
@@ -0,0 +1,11 @@
+
+/** homework **/
+
+public class Sorter {
+	public static void main(String[] args) {
+		String[] strArr = {"Buff", "report.txt", "address.txt", "Homework.java"};
+		for(int i = 1; i < strArr.length; i++) {
+			Thread t = new Thread();
+		}
+	}
+}
